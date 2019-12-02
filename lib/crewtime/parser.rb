02a93a874fb0d13cc352eb5a94ba7d9b029e436a
Jens Neuhalfen <jens@neuhalfen.name>
2019-12-02 15:26:37 +0100

    Initial import of the skeleton project

---
+++ lib/crewtime/parser.rb
@@ -0,0 +1,8 @@
+require "crewtime/parser/version"
+
+module Crewtime
+  module Parser
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/crewtime/parser/version.rb
@@ -0,0 +1,5 @@
+module Crewtime
+  module Parser
+    VERSION = "0.1.0"
+  end
+end
